6823f5e7e1515bfc8e5ddf9a10ca6027e1925867
Soh1121 <satou.shg@gmail.com>
2021-03-17 17:26:04 +0900

    [update]Iris-setosaとIris-versicolorのラベルを抽出し、散布図を使ってデータを可視化 #2

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -1,4 +1,7 @@
 import numpy as np
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class Perceptron:
@@ -27,3 +30,38 @@ class Perceptron:
 
     def predict(self, X):   # 活性化関数はステップ関数
         return np.where(0.0 <= self.net_input(X), 1, -1)
+
+v1 = np.array([1, 2, 3])
+v2 = 0.5 * v1
+# print(np.arccos(v1.dot(v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))))
+
+#### 2.3
+a = np.arange(3)
+# print(a)
+b = np.arange(3, 6)
+# print(b)
+# print(a * b)
+
+s = os.path.join(
+    "https://archive.ics.uci.edu",
+    "ml",
+    "machine-learning-databases",
+    "iris",
+    "iris.data")
+# print("URL:", s)
+df = pd.read_csv(s, header=None, encoding="utf-8")
+# print(df.tail())
+
+y = df.iloc[:100, 4].values
+y = np.where(y == "Iris-versicolor", 1, -1)
+# print(y)
+
+X = df.iloc[:100, [0, 2]].values
+# print(x)
+
+plt.scatter(X[:50, 0], X[:50, 1], color="red", marker="o", label="setosa")
+plt.scatter(X[50:100, 0], X[50:100, 1], color="blue", marker="x", label="versicolor")
+plt.xlabel="sepal length [cm]"
+plt.ylabel="sepal width [cm]"
+plt.legend(loc="upper left")
+plt.show()
