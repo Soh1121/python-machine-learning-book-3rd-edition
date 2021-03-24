323c1ab0573b6bc7b1e99d001911194cca0489f7
Soh1121 <satou.shg@gmail.com>
2021-03-24 13:29:24 +0900

    [update]2次元データセットの決定境界を可視化 #4

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
+
 class Perceptron:
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
@@ -39,13 +40,36 @@ def plot_decision_region(X, y, classifier, resolution=0.02):
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
     # 決定領域のプロット
-    x1_min, x1_max = X[: 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[: 1].min() - 1, X[:, 1].max() + 1
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 
     # グリッドポイントの生成
     xx1, xx2 = np.meshgrid(
         np.arange(x1_min, x1_max, resolution),
         np.arange(x2_min, x2_max, resolution))
+
+    # 各特徴量を1次元配列に変換して予測を実行
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+
+    # 予測結果を元のグリッドポイントのデータサイズに変換
+    Z = Z.reshape(xx1.shape)
+
+    # グリッドポイントの等高線のプロット
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+
+    # 軸の範囲の固定
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # クラスごとに訓練データをプロット
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolor="black")
 
 
 v1 = np.array([1, 2, 3])
@@ -99,3 +123,16 @@ ppn.fit(X, y)
 # 図の表示
 # plt.show()
 
+
+# 決定領域のプロット
+plot_decision_region(X, y, classifier=ppn)
+
+# 軸のラベルの設定
+plt.xlabel("sepal length [cm]")
+plt.ylabel("petal length [cm]")
+
+# 凡例の設定
+plt.legend(loc="upper left")
+
+# 図の表示
+plt.show()
