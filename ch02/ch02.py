b6986cfe1600ef0bf11bb4850ceebf96fc320292
Soh1121 <satou.shg@gmail.com>
2021-03-30 15:04:26 +0900

    [update]標準化データで勾配降下法によるADALINEの学習を実装 #8

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -194,4 +194,29 @@ X_std = np.copy(X)
 # 各列の標準化
 X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
 X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
-print(X_std[:5, :])
+# print(X_std[:5, :])
+
+# 勾配降下法によるADALINEの学習（標準化後、学習率eta=0.01）
+ada_gd = AdalineGD(n_iter=15, eta=0.01)
+# モデルの適合
+ada_gd.fit(X_std, y)
+# 境界領域のプロット
+plot_decision_region(X_std, y, classifier=ada_gd)
+# タイトルの設定
+plt.title("Adaline - Gradient Descent")
+# 軸のラベルの設定
+plt.xlabel("sepal length [standardized]")
+plt.ylabel("petal length [standardized]")
+# 凡例の設定
+plt.legend(loc="upper left")
+# 図の表示
+plt.tight_layout()
+plt.show()
+# エポック数とコストの関係を表す折れ線グラフのプロット
+plt.plot(range(1, len(ada_gd.cost_) + 1), ada_gd.cost_, marker="o")
+# 軸のラベルの設定
+plt.xlabel("Epochs")
+plt.ylabel("Sum-squared-error")
+# 図の表示
+plt.tight_layout()
+plt.show()
