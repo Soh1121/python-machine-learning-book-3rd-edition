5e7928d37f9c32df6aa243ebeab9d1e60cf121c2
Soh1121 <satou.shg@gmail.com>
2021-04-06 13:38:01 +0900

    [update]AdalineSGD分類器の訓練と結果のプロット #10

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -89,7 +89,7 @@ class AdalinSGD(object):
             self._initialize_weight(X.shape[1])
         if y.ravel().shape[0] > 1:
             for xi, target in zip(X, y):
-                self._update_weight(xi, target)
+                self._update_weights(xi, target)
         else:
             self._update_weight(X, y)
         return self
@@ -103,8 +103,8 @@ class AdalinSGD(object):
         r = self.rgen.permutation(len(y))
         return X[r], y[r]
 
-    def _update_weight(self, xi, target):
-        output = self.activation(xi)
+    def _update_weights(self, xi, target):
+        output = self.activation(self.net_input(xi))
         error = target - output
         self.w_[1:] += self.eta * xi.dot(error)
         self.w_[0] += self.eta * error
@@ -278,3 +278,20 @@ X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
 # # 図の表示
 # plt.tight_layout()
 # plt.show()
+
+# 確率的勾配降下法によるADALINEの学習
+ada_sgd = AdalinSGD(n_iter=15, eta=0.01, random_state=1)
+# モデルへの適合
+ada_sgd.fit(X_std, y)
+# 境界領域のプロット
+plot_decision_region(X_std, y, classifier=ada_sgd)
+# タイトルの設定
+plt.title("Adaline - Stochastic Gradient Descent")
+# 軸のラベルの設定
+plt.xlabel("sepal length [standardized]")
+plt.ylabel("petal length [standardized]")
+# 凡例の設定（左上）
+plt.legend(loc="upper left")
+# プロットの表示
+plt.tight_layout()
+plt.show()
