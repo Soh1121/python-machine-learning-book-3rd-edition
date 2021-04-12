ecc778b8431b3ae9181e9c01bf9de08ddfe2b857
Soh1121 <satou.shg@gmail.com>
2021-04-12 16:25:10 +0900

    [update]特徴量を標準化 #15

--- ch03/ch03.py
+++ ch03/ch03.py
@@ -1,6 +1,8 @@
 from sklearn import datasets
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
@@ -11,10 +13,18 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
 # データセットのクラスラベルを確認
-print("Label counts in y: ", np.bincount(y))
+# print("Label counts in y: ", np.bincount(y))
 
 # 訓練データセットのクラスラベルを確認
-print("Label counts in y_train: ", np.bincount(y_train))
+# print("Label counts in y_train: ", np.bincount(y_train))
 
 # テストデータセットのクラスラベルを確認
-print("Label counts in y_test", np.bincount(y_test))
+# print("Label counts in y_test", np.bincount(y_test))
+
+# インスタンス変数scにStandardScalerクラスを代入
+sc = StandardScaler()
+# 訓練データの平均と標準偏差を計算
+sc.fit(X_train)
+# 平均と標準偏差を用いて標準化
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
