bac997c2d532faf0e3855372820e6e15e01c1bf4
Soh1121 <satou.shg@gmail.com>
2021-04-12 16:17:13 +0900

    [update]訓練データセットとテストデータセットに分割 #13

--- ch03/ch03.py
+++ ch03/ch03.py
@@ -1,8 +1,11 @@
 from sklearn import datasets
 import numpy as np
-
+from sklearn.model_selection import train_test_split
 
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
 y = iris.target
-print("Class labels:", np.unique(y))
+# print("Class labels:", np.unique(y))
+
+# 全体の30%がテストデータとなるよう訓練データとテストデータに分割
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
