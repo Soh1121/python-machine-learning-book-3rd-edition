f9ca7bae150d03746d3e795cd4ae561418ab2cff
Soh1121 <satou.shg@gmail.com>
2021-05-10 10:51:22 +0900

    [update]正則化の強さを可視化

--- ch03/ch03.py
+++ ch03/ch03.py
@@ -235,12 +235,37 @@ lr.fit(X_train_std, y_train)
 # plt.tight_layout()
 # plt.show()
 
-# クラス予測を実施
-# クラスの所属確率を算出
-print(lr.predict_proba(X_test_std[:3, :]))
-# クラスのラベルの予測値を算出
-print(lr.predict_proba(X_test_std[:3, :]).argmax(axis=1))
-# 最尤のクラスラベルを表示
-print(lr.predict(X_test_std[:3, :]))
-# 1行のデータの場合は2次元配列に変換して表示
-print(lr.predict(X_test_std[0, :].reshape(1, -1)))
+# # クラス予測を実施
+# # クラスの所属確率を算出
+# print(lr.predict_proba(X_test_std[:3, :]))
+# # クラスのラベルの予測値を算出
+# print(lr.predict_proba(X_test_std[:3, :]).argmax(axis=1))
+# # 最尤のクラスラベルを表示
+# print(lr.predict(X_test_std[:3, :]))
+# # 1行のデータの場合は2次元配列に変換して表示
+# print(lr.predict(X_test_std[0, :].reshape(1, -1)))
+
+# 正則化の強さを可視化する
+# 空のリストを生成（重み係数、逆正則化パラメータ）
+weights, params = [], []
+# 10個の逆正則化パラメータに対応するロジスティック回帰モデルをそれぞれ処理
+for c in np.arange(-5, 5):
+    # ロジスティック回帰モデルを生成
+    lr = LogisticRegression(C=10.**c, random_state=1, solver='lbfgs', multi_class='ovr')
+    # 学習を実行
+    lr.fit(X_train_std, y_train)
+    # 重み係数を格納
+    weights.append(lr.coef_[1])
+    # 逆正則化パラメータを格納
+    params.append(10.**c)
+# 重み係数をNumpy配列に変換
+weights = np.array(weights)
+# 横軸に逆正則化パラメータ、縦軸に重み係数をプロット
+plt.plot(params, weights[:, 0], label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--', label='petal width')
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+# 横軸を対数スケールに設定
+plt.xscale('log')
+plt.show()
