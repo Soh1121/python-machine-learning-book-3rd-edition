52eaf67ccaa25b59a5497d3f1ba668042c0b0815
Soh1121 <satou.shg@gmail.com>
2021-05-19 22:18:21 +0900

    [update]scikit-learnを用いた欠測値の補完を実装

--- ch04/ch04.py
+++ ch04/ch04.py
@@ -2,6 +2,10 @@
 import pandas as pd
 # CSVフォーマットのデータを読み込むためにioモジュールからStringIOをインポートする
 from io import StringIO
+# 欠測値を補完するためにscikit-learnのimputeモジュールからSimpleImputerクラスをインポートする
+from sklearn.impute import SimpleImputer
+# ndarayを扱うためにnumpyをnpとしてインポートする
+import numpy as np
 
 
 # 表形式のデータで欠損値を見てみる
@@ -26,6 +30,15 @@ df = pd.read_csv(StringIO(csv_data))
 # すべての列がNaNである行だけを削除
 # print(df.dropna(how='all'))
 # 非NaNの値が4つ未満の行を削除
-print(df.dropna(thresh=4))
+# print(df.dropna(thresh=4))
 # 特定の列にNaNが含まれている行だけ削除
-print(df.dropna(subset=['C']))
+# print(df.dropna(subset=['C']))
+
+# 欠測値をscikit-learnを用いて補完する
+# 欠測値補完のインスタンスを生成（平均値補完）
+imr = SimpleImputer(missing_values=np.nan, strategy='mean')
+# データを適合
+imr.fit(df.values)
+# 補完を実行
+imputed_data = imr.transform(df.values)
+print(imputed_data)
