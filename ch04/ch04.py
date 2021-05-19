f7932d39ca703dd501f4480a5fb8ad271f856753
Soh1121 <satou.shg@gmail.com>
2021-05-19 22:07:53 +0900

    [update]各特徴量の欠測値をカウントする処理を実装

--- ch04/ch04.py
+++ ch04/ch04.py
@@ -6,10 +6,13 @@ from io import StringIO
 
 # 表形式のデータで欠損値を見てみる
 # サンプルデータを作成
-csv_data = '''A, B, C, D
-              1.0, 2.0, 3.0, 4.0
-              5.0, 6.0, , 8.0
-              10.0, 11.0, 12.0, '''
+csv_data = '''A,B,C,D
+              1.0,2.0,3.0,4.0
+              5.0,6.0,,8.0
+              10.0,11.0,12.0,'''
 # サンプルデータを読み込む
 df = pd.read_csv(StringIO(csv_data))
-print(df)
+# print(df)
+
+# 各特徴量の欠測値をカウント
+print(df.isnull().sum())
