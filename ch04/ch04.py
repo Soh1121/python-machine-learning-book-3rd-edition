6a1071d3853cadfa1b5da28f3e94784073c5aa5b
Soh1121 <satou.shg@gmail.com>
2021-05-29 19:34:47 +0900

    [update]正則化パスをプロット

--- ch04/ch04.py
+++ ch04/ch04.py
@@ -18,6 +18,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 # L1正則化をロジスティック回帰で用いるためにscikit-learnのlinear_modelモジュールからLogisticRegressionをインポート
 from sklearn.linear_model import LogisticRegression
+# 正則化パスをプロットするためにmatplotlibのpyplotモジュールをpltとしてインポート
+import matplotlib.pyplot as plt
 # ndarayを扱うためにnumpyをnpとしてインポートする
 import numpy as np
 
@@ -183,19 +185,58 @@ X_test_std = stdsc.fit_transform(X_test)
 # L1正則化ロジスティック回帰のインスタンスを生成
 # LogisticRegression(penalty='l1', solver='liblinear', multi_class='ovr')
 
-# L1正則化ロジスティック回帰のインスタンスを生成：逆正則化パラメータC=1.0はデフォルト値であり、
-# 値を大きくしたり小さくしたりすると、正則化の効果を強めたり弱めたりできる
-lr = LogisticRegression(penalty='l1', C=1.0, solver='liblinear', multi_class='ovr')
-# 訓練データに適合
-lr.fit(X_train_std, y_train)
-# 訓練データに対する正解率の表示
-# print('Training accuracy:', lr.score(X_train_std, y_train))
+# # L1正則化ロジスティック回帰のインスタンスを生成：逆正則化パラメータC=1.0はデフォルト値であり、
+# # 値を大きくしたり小さくしたりすると、正則化の効果を強めたり弱めたりできる
+# lr = LogisticRegression(penalty='l1', C=1.0, solver='liblinear', multi_class='ovr')
+# # 訓練データに適合
+# lr.fit(X_train_std, y_train)
+# # 訓練データに対する正解率の表示
+# # print('Training accuracy:', lr.score(X_train_std, y_train))
 
-# テストデータに対する正解率の表示
-# print('Test accuracy:', lr.score(X_test_std, y_test))
+# # テストデータに対する正解率の表示
+# # print('Test accuracy:', lr.score(X_test_std, y_test))
 
-# モデルの切片を確認
-# print(lr.intercept_)
+# # モデルの切片を確認
+# # print(lr.intercept_)
 
-# モデルの重み係数の表示
-print(lr.coef_)
+# # モデルの重み係数の表示
+# print(lr.coef_)
+
+# 描画の準備
+fig = plt.figure()
+ax = plt.subplot(111)
+# 各係数の色のリスト
+colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black',
+          'pink', 'lightgreen', 'lightblue', 'gray', 'indigo', 'orange']
+# 空のリストを生成（重み係数、逆正則化パラメータ）
+weights, params = [], []
+# 逆正則化パラメータの値ごとに処理
+for c in np.arange(-4., 6.):
+    lr = LogisticRegression(penalty='l1', C=10.**c, solver='liblinear', multi_class='ovr', random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+# 重み係数をNumPy配列に変換
+weights = np.array(weights)
+# 各重み係数をプロット
+for column, color in zip(range(weights.shape[1]), colors):
+    # 横軸を逆正則化パラメータ、縦軸を重み係数とした主線グラフ
+    plt.plot(params, weights[:, column], label=df_wine.columns[column+1], color=color)
+
+# y=0に黒い波線を引く
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+# 横軸の範囲の設定
+plt.xlim([10**(-5), 10**5])
+# 軸のラベルを設定
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+# 横軸を対数スケールに設定
+plt.xscale('log')
+# 凡例を表示
+plt.legend()
+# グラフ外に凡例を移動
+ax.legend(loc='upper center', bbox_to_anchor=(1.38, 1.03), ncol=1, fancybox=True)
+# プロットの表示
+plt.tight_layout()
+plt.show()
