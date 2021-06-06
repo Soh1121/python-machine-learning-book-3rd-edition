54d40ec5ea7990e172f5165a4eb1e16edd8c8668
Soh1121 <satou.shg@gmail.com>
2021-06-06 13:32:54 +0900

    [update]PCAによる変換データをプロットしたものを表示

--- ch05/ch05.py
+++ ch05/ch05.py
@@ -61,4 +61,23 @@ w = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis
 # print('Matrix W:\n', w)
 
 # 0行目のデータに対し、射影行列を用いてデータを変換
-print(X_train_std[0].dot(w))
+# print(X_train_std[0].dot(w))
+
+# データ全体を射影行列を用いてデータ変換
+X_train_pca = X_train_std.dot(w)
+
+# 変換後の訓練データセットをプロット
+# 色とマーカーの設定
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+# 「クラスラベル」「点の色」「点の種類」の組み合わせからなるリストを生成してプロット
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train==l, 0], X_train_pca[y_train==l, 1], c=c, label=l, marker=m)
+# 軸のラベルを設定
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+# 凡例を左下に表示
+plt.legend(loc='lower left')
+# プロットを表示
+plt.tight_layout()
+plt.show()
