03707729db82df84651cc601a1ad8c79c884dbb8
Soh1121 <satou.shg@gmail.com>
2021-06-09 16:29:37 +0900

    [update]散布図で正しくプロットされているか確認

--- ch05/ch05.py
+++ ch05/ch05.py
@@ -501,3 +501,20 @@ def project_x(x_new, X, gamma, alphas, lambdas):
     pair_dist = np.array([np.sum((x_new - row) ** 2) for row in X])
     k = np.exp(-gamma * pair_dist)
     return k.dot(alphas / lambdas)
+
+
+x_reproj = project_x(x_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
+print(x_reproj)
+
+# 最初の主成分への射影をプロット
+plt.scatter(alphas[y == 0, 0], np.zeros((50)), color='red', marker='^', alpha=0.5)
+plt.scatter(alphas[y == 1, 0], np.zeros((50)), color='blue', marker='o', alpha=0.5)
+plt.scatter(x_proj, 0, color='black', label='Original projection of point X[25]', marker='^', s=100)
+plt.scatter(x_reproj, 0, color='green', label='Remapped point X[25]', marker='x', s=500)
+# 目盛りを削除
+plt.yticks([], [])
+# 凡例を右上に表示
+plt.legend(scatterpoints=1)
+# プロットを表示
+plt.tight_layout()
+plt.show()
