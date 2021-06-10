7f0d02b41bfbbbe852930e286a789f706574a784
Soh1121 <satou.shg@gmail.com>
2021-06-10 19:35:46 +0900

    [update]scikit-learnを用いて層化k分割交差検証を実行

--- ch06/ch06.py
+++ ch06/ch06.py
@@ -16,6 +16,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 # 要素数をカウントするためなどにnumpyをnpとしてインポート
 import numpy as np
+# 交差検証の正解率を算出するため、scikit-learnのmodel_selectionモジュールからcross_val_scoreをインポート
+from sklearn.model_selection import cross_val_score
 
 
 # UCIのWebサイトからpandasライブラリのread_csv関数を使ってデータセットを直接読み込む
@@ -48,19 +50,27 @@ pipe_lr.fit(X_train, y_train)
 pipe_lr.predict(X_test)
 # print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
 
-# 層化k分割交差検証を試行
-# 分割元データ、分割数、乱数生成器の状態を指定し、層化k分割交差検証イテレータを表すStratifriedKFoldクラスのインスタンス化
-kfold = StratifiedKFold(n_splits=10).split(X_train, y_train)
-scores = []
-# イテレータのインデックスと要素をループ処理：
-for k, (train, test) in enumerate(kfold):
-    # データをモデルに適合
-    pipe_lr.fit(X_train[train], y_train[train])
-    # テストデータの正解率を算出
-    score = pipe_lr.score(X_train[test], y_train[test])
-    # リストに正解率を追加
-    scores.append(score)
-    # 分割の番号、0以上の要素数、正解率を出力
-    print('Fold: %2d, Class dist.: %s, Acc: %.3f' % (k + 1, np.bincount(y_train[train]), score))
-# 正解率の平均と標準偏差を出力
-print('\nCV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+# # 層化k分割交差検証を試行
+# # 分割元データ、分割数、乱数生成器の状態を指定し、層化k分割交差検証イテレータを表すStratifriedKFoldクラスのインスタンス化
+# kfold = StratifiedKFold(n_splits=10).split(X_train, y_train)
+# scores = []
+# # イテレータのインデックスと要素をループ処理：
+# for k, (train, test) in enumerate(kfold):
+#     # データをモデルに適合
+#     pipe_lr.fit(X_train[train], y_train[train])
+#     # テストデータの正解率を算出
+#     score = pipe_lr.score(X_train[test], y_train[test])
+#     # リストに正解率を追加
+#     scores.append(score)
+#     # 分割の番号、0以上の要素数、正解率を出力
+#     print('Fold: %2d, Class dist.: %s, Acc: %.3f' % (k + 1, np.bincount(y_train[train]), score))
+# # 正解率の平均と標準偏差を出力
+# print('\nCV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+# scikit-learnを用いて層化k分割交差検証を実行
+# 推定器estimator、訓練データX、予測値y、分割数cv、CPU数n_jobsを指定
+scores = cross_val_score(estimator=pipe_lr,
+                         X=X_train, y=y_train,
+                         cv=10, n_jobs=1)
+print('CV accuracy scores: %s' % scores)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
