7675b4bb988bdcba05a657e01e887a36ba08bd51
Soh1121 <satou.shg@gmail.com>
2021-06-10 19:30:52 +0900

    [update]層化k分割交差検証を実装

--- ch06/ch06.py
+++ ch06/ch06.py
@@ -12,6 +12,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 # ロジスティック回帰を用いるため、scikit-learnのlinear_modelモジュールからLogisticRegressionをインポート
 from sklearn.linear_model import LogisticRegression
+# 層化k分割交差検証を行うため、scikit-learnのmodel_selectionモジュールからStratifiedKFoldをインポート
+from sklearn.model_selection import StratifiedKFold
+# 要素数をカウントするためなどにnumpyをnpとしてインポート
+import numpy as np
 
 
 # UCIのWebサイトからpandasライブラリのread_csv関数を使ってデータセットを直接読み込む
@@ -42,4 +46,21 @@ pipe_lr = make_pipeline(StandardScaler(),
 pipe_lr.fit(X_train, y_train)
 # パイプラインで推定を実行
 pipe_lr.predict(X_test)
-print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
+# print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
+
+# 層化k分割交差検証を試行
+# 分割元データ、分割数、乱数生成器の状態を指定し、層化k分割交差検証イテレータを表すStratifriedKFoldクラスのインスタンス化
+kfold = StratifiedKFold(n_splits=10).split(X_train, y_train)
+scores = []
+# イテレータのインデックスと要素をループ処理：
+for k, (train, test) in enumerate(kfold):
+    # データをモデルに適合
+    pipe_lr.fit(X_train[train], y_train[train])
+    # テストデータの正解率を算出
+    score = pipe_lr.score(X_train[test], y_train[test])
+    # リストに正解率を追加
+    scores.append(score)
+    # 分割の番号、0以上の要素数、正解率を出力
+    print('Fold: %2d, Class dist.: %s, Acc: %.3f' % (k + 1, np.bincount(y_train[train]), score))
+# 正解率の平均と標準偏差を出力
+print('\nCV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
