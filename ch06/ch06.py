967bf7afce57da40b54d5dc56cf08867ab3c2129
Soh1121 <satou.shg@gmail.com>
2021-06-15 21:55:46 +0900

    [update]混同行列を図示

--- ch06/ch06.py
+++ ch06/ch06.py
@@ -211,4 +211,19 @@ pipe_svc.fit(X_train, y_train)
 y_pred = pipe_svc.predict(X_test)
 # テストと予測のデータから混同行列を生成
 confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
-print(confmat)
+# print(confmat)
+
+# 混同行列を図示
+# 図のサイズを指定
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
+# matshow関数で行列からヒートマップを作成
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+for i in range(confmat.shape[0]):       # クラス0の繰り返し処理
+    for j in range(confmat.shape[1]):   # クラス1の繰り返し処理
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')    # 件数を表示
+# 軸のラベルを設定
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+# プロットを表示
+plt.tight_layout()
+plt.show()
