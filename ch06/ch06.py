4e07f95624c7ebeb4666eee455eaafddc8ced1e7
Soh1121 <satou.shg@gmail.com>
2021-06-16 16:52:59 +0900

    [update]マイクロ平均を性能指標に指定する例を実装

--- ch06/ch06.py
+++ ch06/ch06.py
@@ -264,51 +264,57 @@ y_pred = pipe_svc.predict(X_test)
 # # パラメータを出力
 # print(gs.best_params_)
 
-# ROC曲線を確認
-# スケーリング、主成分分析、ロジスティック回帰を指定して、Pipelineクラスをインスタンス化
-pipe_lr = make_pipeline(StandardScaler(), PCA(n_components=2),
-                        LogisticRegression(penalty='l2', random_state=1,
-                                           solver='lbfgs', C=100.0))
-# 2つの特徴量を抽出
-X_train2 = X_train[:, [4, 14]]
-# 層化k分割交差検証イテレータを表すStrratifiedKFoldクラスをインスタンス化
-cv = list(StratifiedKFold(n_splits=3).split(X_train, y_train))
-# 作画領域を指定
-fig = plt.figure(figsize=(7, 5))
-# TPRの平均値を初期化
-mean_tpr = 0.0
-# 0から1までの間で100個の要素を生成
-mean_fpr = np.linspace(0, 1, 100)
-# すべてのTPRを記憶するリストを初期化
-all_tpr = []
-for i, (train, test) in enumerate(cv):
-    # predict_probaメソッドで確率を予測、fitメソッドでモデルに適合させる
-    probas = pipe_lr.fit(X_train2[train],
-                         y_train[train]).predict_proba(X_train2[test])
-    # roc_curve関数でROC曲線の性能を計算してプロット
-    fpr, tpr, thresholds = roc_curve(y_train[test], probas[:, 1], pos_label=1)
-    mean_tpr += interp(mean_fpr, fpr, tpr)  # FPR（X軸）とTPR（Y軸）を線形補間
-    mean_tpr[0] = 0.0
-    roc_auc = auc(fpr, tpr)                 # 曲線下面積（AUC）を計算
-    plt.plot(fpr, tpr, label='ROC fold %d (area = %0.2f)' % (i + 1, roc_auc))
+# # ROC曲線を確認
+# # スケーリング、主成分分析、ロジスティック回帰を指定して、Pipelineクラスをインスタンス化
+# pipe_lr = make_pipeline(StandardScaler(), PCA(n_components=2),
+#                         LogisticRegression(penalty='l2', random_state=1,
+#                                            solver='lbfgs', C=100.0))
+# # 2つの特徴量を抽出
+# X_train2 = X_train[:, [4, 14]]
+# # 層化k分割交差検証イテレータを表すStrratifiedKFoldクラスをインスタンス化
+# cv = list(StratifiedKFold(n_splits=3).split(X_train, y_train))
+# # 作画領域を指定
+# fig = plt.figure(figsize=(7, 5))
+# # TPRの平均値を初期化
+# mean_tpr = 0.0
+# # 0から1までの間で100個の要素を生成
+# mean_fpr = np.linspace(0, 1, 100)
+# # すべてのTPRを記憶するリストを初期化
+# all_tpr = []
+# for i, (train, test) in enumerate(cv):
+#     # predict_probaメソッドで確率を予測、fitメソッドでモデルに適合させる
+#     probas = pipe_lr.fit(X_train2[train],
+#                          y_train[train]).predict_proba(X_train2[test])
+#     # roc_curve関数でROC曲線の性能を計算してプロット
+#     fpr, tpr, thresholds = roc_curve(y_train[test], probas[:, 1], pos_label=1)
+#     mean_tpr += interp(mean_fpr, fpr, tpr)  # FPR（X軸）とTPR（Y軸）を線形補間
+#     mean_tpr[0] = 0.0
+#     roc_auc = auc(fpr, tpr)                 # 曲線下面積（AUC）を計算
+#     plt.plot(fpr, tpr, label='ROC fold %d (area = %0.2f)' % (i + 1, roc_auc))
 
-# 当て推量をプロット
-plt.plot([0, 1], [0, 1], linestyle='--', color=(0.6, 0.6, 0.6), label='Random guessing')
-# FPR、TPR、ROC AUCそれぞれの平均を計算してプロット
-mean_tpr /= len(cv)
-mean_tpr[-1] = 1.0
-mean_auc = auc(mean_fpr, mean_tpr)
-plt.plot(mean_fpr, mean_tpr, 'k--', label='Mean ROC (area = %0.2f)' % mean_auc, lw=2)
-# 完全に予測が正解したときのROC曲線をプロット
-plt.plot([0, 0, 1], [0, 1, 1], linestyle=':', color='black', label='Perfect performance')
-# 軸の上限・下限を設定
-plt.xlim([-0.05, 1.05])
-plt.ylim([-0.05, 1.05])
-# 軸のラベルを設定
-plt.xlabel('False positive rate')
-plt.ylabel('True positive rate')
-# 凡例を右下に設定
-plt.legend(loc="lower right")
-# プロットを表示
-plt.tight_layout()
-plt.show()
+# # 当て推量をプロット
+# plt.plot([0, 1], [0, 1], linestyle='--', color=(0.6, 0.6, 0.6), label='Random guessing')
+# # FPR、TPR、ROC AUCそれぞれの平均を計算してプロット
+# mean_tpr /= len(cv)
+# mean_tpr[-1] = 1.0
+# mean_auc = auc(mean_fpr, mean_tpr)
+# plt.plot(mean_fpr, mean_tpr, 'k--', label='Mean ROC (area = %0.2f)' % mean_auc, lw=2)
+# # 完全に予測が正解したときのROC曲線をプロット
+# plt.plot([0, 0, 1], [0, 1, 1], linestyle=':', color='black', label='Perfect performance')
+# # 軸の上限・下限を設定
+# plt.xlim([-0.05, 1.05])
+# plt.ylim([-0.05, 1.05])
+# # 軸のラベルを設定
+# plt.xlabel('False positive rate')
+# plt.ylabel('True positive rate')
+# # 凡例を右下に設定
+# plt.legend(loc="lower right")
+# # プロットを表示
+# plt.tight_layout()
+# plt.show()
+
+# マイクロ平均を性能指標に指定
+pre_scorer = make_scorer(score_func=precision_score,
+                         pos_label=1,
+                         greater_is_better=True,
+                         average='micro')
