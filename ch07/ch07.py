3819f0f1823bd36411fea0f1a4192ae6f047e401
Soh1121 <satou.shg@gmail.com>
2021-10-26 21:56:59 +0900

    [update]個々の分類器にアクセスする方法を確認

--- ch07/ch07.py
+++ ch07/ch07.py
@@ -260,47 +260,50 @@ for clf, label in zip(all_clf, clf_labels):
 # plt.ylabel('True positive rate (TPR)')
 # plt.show()
 
-# 標準化を行うため、Scalerを用意
-sc = StandardScaler()
-# 学習データに標準化を適用
-X_train_std = sc.fit_transform(X_train)
-# 決定領域を描画する最小値、最大値を生成
-x_min = X_train_std[:, 0].min() - 1
-x_max = X_train_std[:, 0].max() + 1
-y_min = X_train_std[:, 1].min() - 1
-y_max = X_train_std[:, 1].max() + 1
-# グリッドポイントを生成
-xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
-                     np.arange(y_min, y_max, 0.1))
-# 描画領域を2行2列に分割
-f, axarr = plt.subplots(nrows=2, ncols=2,
-                        sharex='col',
-                        sharey='row',
-                        figsize=(7, 5))
-# 決定領域のプロット、青や赤の散布図の作成などを実行
-# 変数idxは各分類器を描画する行と列の位置を表すタプル
-for idx, clf, tt in zip(product([0, 1], [0, 1]), all_clf, clf_labels):
-    clf.fit(X_train_std, y_train)
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.3)
-    axarr[idx[0], idx[1]].scatter(X_train_std[y_train==0, 0],
-                                  X_train_std[y_train==0, 1],
-                                  c='blue',
-                                  marker='^',
-                                  s=50)
-    axarr[idx[0], idx[1]].scatter(X_train_std[y_train==1, 0],
-                                  X_train_std[y_train==1, 1],
-                                  c='green',
-                                  marker='o',
-                                  s=50)
-    axarr[idx[0], idx[1]].set_title(tt)
+# # 標準化を行うため、Scalerを用意
+# sc = StandardScaler()
+# # 学習データに標準化を適用
+# X_train_std = sc.fit_transform(X_train)
+# # 決定領域を描画する最小値、最大値を生成
+# x_min = X_train_std[:, 0].min() - 1
+# x_max = X_train_std[:, 0].max() + 1
+# y_min = X_train_std[:, 1].min() - 1
+# y_max = X_train_std[:, 1].max() + 1
+# # グリッドポイントを生成
+# xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+#                      np.arange(y_min, y_max, 0.1))
+# # 描画領域を2行2列に分割
+# f, axarr = plt.subplots(nrows=2, ncols=2,
+#                         sharex='col',
+#                         sharey='row',
+#                         figsize=(7, 5))
+# # 決定領域のプロット、青や赤の散布図の作成などを実行
+# # 変数idxは各分類器を描画する行と列の位置を表すタプル
+# for idx, clf, tt in zip(product([0, 1], [0, 1]), all_clf, clf_labels):
+#     clf.fit(X_train_std, y_train)
+#     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+#     Z = Z.reshape(xx.shape)
+#     axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.3)
+#     axarr[idx[0], idx[1]].scatter(X_train_std[y_train==0, 0],
+#                                   X_train_std[y_train==0, 1],
+#                                   c='blue',
+#                                   marker='^',
+#                                   s=50)
+#     axarr[idx[0], idx[1]].scatter(X_train_std[y_train==1, 0],
+#                                   X_train_std[y_train==1, 1],
+#                                   c='green',
+#                                   marker='o',
+#                                   s=50)
+#     axarr[idx[0], idx[1]].set_title(tt)
 
-plt.text(-.35, -5.,
-         s='Sepal width [standardized]',
-         ha='center', va='center', fontsize=12)
-plt.text(-12.5, 4.5,
-         s='Petal length [standardized]',
-         ha='center', va='center',
-         fontsize=12, rotation=90)
-plt.show()
+# plt.text(-.35, -5.,
+#          s='Sepal width [standardized]',
+#          ha='center', va='center', fontsize=12)
+# plt.text(-12.5, 4.5,
+#          s='Petal length [standardized]',
+#          ha='center', va='center',
+#          fontsize=12, rotation=90)
+# plt.show()
+
+# GridSearchCVオブジェクトの個々のパラメータを見てみる
+print(mv_clf.get_params())
