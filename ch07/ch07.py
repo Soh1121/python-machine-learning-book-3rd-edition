03c48783a5ef3110d4fc24ce120899dbeeaa6763
Soh1121 <satou.shg@gmail.com>
2021-10-31 11:56:52 +0900

    [update]バギングを使ってWineデータセットのデータ点を分類

--- ch07/ch07.py
+++ ch07/ch07.py
@@ -37,6 +37,12 @@ from sklearn.metrics import auc
 from itertools import product
 # ロジスティック回帰分類器の逆正則化パラメータと決定木の深さをチューニングするため、sklearnのmodel_selectionクラスから、GridSearchCVをインポートする
 from sklearn.model_selection import GridSearchCV
+# Wineデータセットを用いるため、pandasをpdとしてインポート
+import pandas as pd
+# バギング分類器を用いるために、sklearnのensembleから、BaggingClassifierをインポートする
+from sklearn.ensemble import BaggingClassifier
+# 性能を算出するため、sklearnのmetricsから、accuracy_scoreメソッドをインポート
+from sklearn.metrics import accuracy_score
 
 from sklearn.base import clone
 
@@ -199,131 +205,206 @@ class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
 # plt.show()
 
 
-iris = datasets.load_iris()
-X, y = iris.data[50:, [1, 2]], iris.target[50:]
-le = LabelEncoder()
-y = le.fit_transform(y)
-X_train, X_test, y_train, y_test = \
-    train_test_split(X, y, test_size=0.5, random_state=1, stratify=y)
-clf1 = LogisticRegression(penalty='l2', C=0.001,
-                          solver='lbfgs',
-                          random_state=1)
-clf2 = DecisionTreeClassifier(max_depth=1,
-                              criterion='entropy',
-                              random_state=0)
-clf3 = KNeighborsClassifier(n_neighbors=1,
-                            p=2,
-                            metric='minkowski')
-pipe1 = Pipeline([['sc', StandardScaler()],
-                  ['clf', clf1]])
-pipe3 = Pipeline([['sc', StandardScaler()],
-                  ['clf', clf3]])
-clf_labels = ['Logistic regression', 'Decision tree', 'KNN']
-print('10-fold cross validation:\n')
-# for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
+# iris = datasets.load_iris()
+# X, y = iris.data[50:, [1, 2]], iris.target[50:]
+# le = LabelEncoder()
+# y = le.fit_transform(y)
+# X_train, X_test, y_train, y_test = \
+#     train_test_split(X, y, test_size=0.5, random_state=1, stratify=y)
+# clf1 = LogisticRegression(penalty='l2', C=0.001,
+#                           solver='lbfgs',
+#                           random_state=1)
+# clf2 = DecisionTreeClassifier(max_depth=1,
+#                               criterion='entropy',
+#                               random_state=0)
+# clf3 = KNeighborsClassifier(n_neighbors=1,
+#                             p=2,
+#                             metric='minkowski')
+# pipe1 = Pipeline([['sc', StandardScaler()],
+#                   ['clf', clf1]])
+# pipe3 = Pipeline([['sc', StandardScaler()],
+#                   ['clf', clf3]])
+# clf_labels = ['Logistic regression', 'Decision tree', 'KNN']
+# print('10-fold cross validation:\n')
+# # for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
+# #     scores = cross_val_score(estimator=clf,
+# #                              X=X_train,
+# #                              y=y_train,
+# #                              cv=10,
+# #                              scoring='roc_auc')
+# #     print("ROC AUC: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+# mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
+# clf_labels += ['Majority voting']
+# all_clf = [pipe1, clf2, pipe3, mv_clf]
+# for clf, label in zip(all_clf, clf_labels):
 #     scores = cross_val_score(estimator=clf,
 #                              X=X_train,
 #                              y=y_train,
 #                              cv=10,
 #                              scoring='roc_auc')
 #     print("ROC AUC: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
-mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
-clf_labels += ['Majority voting']
-all_clf = [pipe1, clf2, pipe3, mv_clf]
-for clf, label in zip(all_clf, clf_labels):
-    scores = cross_val_score(estimator=clf,
-                             X=X_train,
-                             y=y_train,
-                             cv=10,
-                             scoring='roc_auc')
-    print("ROC AUC: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
 
-# colors = ['black', 'orange', 'blue', 'green']
-# linestyles = [':', '--', '-.', '-']
-# for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
-#     # 陽性クラスのラベルは1であることが前提
-#     y_pred = clf.fit(X_train, y_train).predict_proba(X_test)[:, 1]
-#     fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
-#     roc_auc = auc(x=fpr, y=tpr)
-#     plt.plot(fpr, tpr,
-#              color=clr,
-#              linestyle=ls,
-#              label='%s (auc = %0.2f)' % (label, roc_auc))
+# # colors = ['black', 'orange', 'blue', 'green']
+# # linestyles = [':', '--', '-.', '-']
+# # for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
+# #     # 陽性クラスのラベルは1であることが前提
+# #     y_pred = clf.fit(X_train, y_train).predict_proba(X_test)[:, 1]
+# #     fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+# #     roc_auc = auc(x=fpr, y=tpr)
+# #     plt.plot(fpr, tpr,
+# #              color=clr,
+# #              linestyle=ls,
+# #              label='%s (auc = %0.2f)' % (label, roc_auc))
 
-# plt.legend(loc='lower right')
-# plt.plot([0, 1], [0, 1],
-#          linestyle='--',
-#          color='gray',
-#          linewidth=2)
-# plt.xlim([-0.1, 1.1])
-# plt.ylim([-0.1, 1.1])
-# plt.grid(alpha=0.5)
-# plt.xlabel('False positive rate (FPR)')
-# plt.ylabel('True positive rate (TPR)')
-# plt.show()
+# # plt.legend(loc='lower right')
+# # plt.plot([0, 1], [0, 1],
+# #          linestyle='--',
+# #          color='gray',
+# #          linewidth=2)
+# # plt.xlim([-0.1, 1.1])
+# # plt.ylim([-0.1, 1.1])
+# # plt.grid(alpha=0.5)
+# # plt.xlabel('False positive rate (FPR)')
+# # plt.ylabel('True positive rate (TPR)')
+# # plt.show()
 
-# # 標準化を行うため、Scalerを用意
-# sc = StandardScaler()
-# # 学習データに標準化を適用
-# X_train_std = sc.fit_transform(X_train)
-# # 決定領域を描画する最小値、最大値を生成
-# x_min = X_train_std[:, 0].min() - 1
-# x_max = X_train_std[:, 0].max() + 1
-# y_min = X_train_std[:, 1].min() - 1
-# y_max = X_train_std[:, 1].max() + 1
-# # グリッドポイントを生成
-# xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
-#                      np.arange(y_min, y_max, 0.1))
-# # 描画領域を2行2列に分割
-# f, axarr = plt.subplots(nrows=2, ncols=2,
-#                         sharex='col',
-#                         sharey='row',
-#                         figsize=(7, 5))
-# # 決定領域のプロット、青や赤の散布図の作成などを実行
-# # 変数idxは各分類器を描画する行と列の位置を表すタプル
-# for idx, clf, tt in zip(product([0, 1], [0, 1]), all_clf, clf_labels):
-#     clf.fit(X_train_std, y_train)
-#     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-#     Z = Z.reshape(xx.shape)
-#     axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.3)
-#     axarr[idx[0], idx[1]].scatter(X_train_std[y_train==0, 0],
-#                                   X_train_std[y_train==0, 1],
-#                                   c='blue',
-#                                   marker='^',
-#                                   s=50)
-#     axarr[idx[0], idx[1]].scatter(X_train_std[y_train==1, 0],
-#                                   X_train_std[y_train==1, 1],
-#                                   c='green',
-#                                   marker='o',
-#                                   s=50)
-#     axarr[idx[0], idx[1]].set_title(tt)
+# # # 標準化を行うため、Scalerを用意
+# # sc = StandardScaler()
+# # # 学習データに標準化を適用
+# # X_train_std = sc.fit_transform(X_train)
+# # # 決定領域を描画する最小値、最大値を生成
+# # x_min = X_train_std[:, 0].min() - 1
+# # x_max = X_train_std[:, 0].max() + 1
+# # y_min = X_train_std[:, 1].min() - 1
+# # y_max = X_train_std[:, 1].max() + 1
+# # # グリッドポイントを生成
+# # xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+# #                      np.arange(y_min, y_max, 0.1))
+# # # 描画領域を2行2列に分割
+# # f, axarr = plt.subplots(nrows=2, ncols=2,
+# #                         sharex='col',
+# #                         sharey='row',
+# #                         figsize=(7, 5))
+# # # 決定領域のプロット、青や赤の散布図の作成などを実行
+# # # 変数idxは各分類器を描画する行と列の位置を表すタプル
+# # for idx, clf, tt in zip(product([0, 1], [0, 1]), all_clf, clf_labels):
+# #     clf.fit(X_train_std, y_train)
+# #     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+# #     Z = Z.reshape(xx.shape)
+# #     axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.3)
+# #     axarr[idx[0], idx[1]].scatter(X_train_std[y_train==0, 0],
+# #                                   X_train_std[y_train==0, 1],
+# #                                   c='blue',
+# #                                   marker='^',
+# #                                   s=50)
+# #     axarr[idx[0], idx[1]].scatter(X_train_std[y_train==1, 0],
+# #                                   X_train_std[y_train==1, 1],
+# #                                   c='green',
+# #                                   marker='o',
+# #                                   s=50)
+# #     axarr[idx[0], idx[1]].set_title(tt)
 
-# plt.text(-.35, -5.,
-#          s='Sepal width [standardized]',
-#          ha='center', va='center', fontsize=12)
-# plt.text(-12.5, 4.5,
-#          s='Petal length [standardized]',
-#          ha='center', va='center',
-#          fontsize=12, rotation=90)
-# plt.show()
+# # plt.text(-.35, -5.,
+# #          s='Sepal width [standardized]',
+# #          ha='center', va='center', fontsize=12)
+# # plt.text(-12.5, 4.5,
+# #          s='Petal length [standardized]',
+# #          ha='center', va='center',
+# #          fontsize=12, rotation=90)
+# # plt.show()
 
-# # GridSearchCVオブジェクトの個々のパラメータを見てみる
-# print(mv_clf.get_params())
+# # # GridSearchCVオブジェクトの個々のパラメータを見てみる
+# # print(mv_clf.get_params())
 
-params = {'decisiontreeclassifier__max_depth': [1, 2],
-          'pipeline-1__clf__C': [0.001, 0.1, 100.0]}
-grid = GridSearchCV(estimator=mv_clf,
-                    param_grid=params,
-                    cv=10,
-                    scoring='roc_auc')
-grid.fit(X_train, y_train)
+# params = {'decisiontreeclassifier__max_depth': [1, 2],
+#           'pipeline-1__clf__C': [0.001, 0.1, 100.0]}
+# grid = GridSearchCV(estimator=mv_clf,
+#                     param_grid=params,
+#                     cv=10,
+#                     scoring='roc_auc')
+# grid.fit(X_train, y_train)
 
-# 10分割交差検証をすることで、様々なハイパーパラメータ値の組み合わせとROC曲線の平均値を出力
-for r, _ in enumerate(grid.cv_results_['mean_test_score']):
-    print("%0.3f +/- %0.2f %r"
-          % (grid.cv_results_['mean_test_score'][r],
-             grid.cv_results_['std_test_score'][r] / 2.0,
-             grid.cv_results_['params'][r]))
+# # 10分割交差検証をすることで、様々なハイパーパラメータ値の組み合わせとROC曲線の平均値を出力
+# for r, _ in enumerate(grid.cv_results_['mean_test_score']):
+#     print("%0.3f +/- %0.2f %r"
+#           % (grid.cv_results_['mean_test_score'][r],
+#              grid.cv_results_['std_test_score'][r] / 2.0,
+#              grid.cv_results_['params'][r]))
 
-print('Best parameters: %s' % grid.best_params_)
-print('Accuracy: %.2f' % grid.best_score_)
+# print('Best parameters: %s' % grid.best_params_)
+# print('Accuracy: %.2f' % grid.best_score_)
+
+# Wineデータセットを抽出
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+# Wineデータのカラムに名前をつける
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids',
+                   'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity',
+                   'Hue', 'OD280/OD315 of diluted wines', 'Proline']
+# クラス2とクラス3のみを分類するためにクラス1を削除
+df_wine = df_wine[df_wine['Class label'] != 1]
+y = df_wine['Class label'].values
+X = df_wine[['Alcohol', 'OD280/OD315 of diluted wines']].values
+
+# ラベルを2値化する
+le = LabelEncoder()
+y = le.fit_transform(y)
+# データセットを80%の訓練データセットと20%のテストデータセットに分ける
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=1,
+                                                    stratify=y)
+# ベース分類器として決定木を利用
+tree = DecisionTreeClassifier(criterion='entropy',
+                              max_depth=None,
+                              random_state=1)
+# バギング分類器を作成
+bag = BaggingClassifier(base_estimator=tree,
+                        n_estimators=500,
+                        max_samples=1.0,
+                        max_features=1.0,
+                        bootstrap=True,
+                        bootstrap_features=False,
+                        n_jobs=1,
+                        random_state=1)
+# 剪定されていない決定木の性能を調べる
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print('Decision tree train/test accuracies %.3f/%.3f' % (tree_train, tree_test))
+# バギング分類器を学習し、性能を調べる
+bag = bag.fit(X_train, y_train)
+y_train_pred = bag.predict(X_train)
+y_test_pred = bag.predict(X_test)
+bag_train = accuracy_score(y_train, y_train_pred)
+bag_test = accuracy_score(y_test, y_test_pred)
+print('Bagging train/test accuracies %.3f/%.3f' % (bag_train, bag_test))
+# 決定領域を比較
+x_min = X_train[:, 0].min() - 1
+x_max = X_train[:, 0].max() + 1
+y_min = X_train[:, 1].min() - 1
+y_max = X_train[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(nrows=1, ncols=2,
+                        sharex='col',
+                        sharey='row',
+                        figsize=(8, 3))
+for idx, clf, tt in zip([0, 1], [tree, bag], ['Decision tree', 'Bagging']):
+    clf.fit(X_train, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx].scatter(X_train[y_train == 0, 0],
+                       X_train[y_train == 0, 1], c='blue', marker='^')
+    axarr[idx].scatter(X_train[y_train == 1, 0],
+                       X_train[y_train == 1, 1], c='green', marker='o')
+    axarr[idx].set_title(tt)
+
+axarr[0].set_ylabel('Alcohol', fontsize=12)
+plt.tight_layout()
+plt.text(0, -0.2, s='Od280.OD315 of diluted wines',
+         ha='center', va='center', fontsize=12, transform=axarr[1].transAxes)
+plt.show()
